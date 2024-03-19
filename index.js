14e9c9ea049e6048e61de1b66e774c85116cf6ef
Abdulwassay122 <143580887+Abdulwassay122@users.noreply.github.com>
2024-03-19 12:09:04 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+#! /usr/bin/env node
+//SHABANG
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "enter forst number", type: "number", name: "firstnumber" },
+    { message: "enter second number", type: "number", name: "secondnumber" },
+    { message: "select any of the operator to perform action",
+        type: "list",
+        name: "operator",
+        choices: ["Addition", "Substraction", "Multiplication", "Division"] }
+]);
+if (answer.operator === "Addition") {
+    console.log(answer.firstnumber + answer.secondnumber);
+}
+else if (answer.operator === "Substraction") {
+    console.log(answer.firstnumber - answer.secondnumber);
+}
+else if (answer.operator === "Multiplication") {
+    console.log(answer.firstnumber * answer.secondnumber);
+}
+else if (answer.operator === "Division") {
+    console.log(answer.firstnumber / answer.secondnumber);
+}
+else {
+    console.log('the operator is not valid');
+}
